034019b6c64eba8b3472f68982b74d71455cd3ad
camou92 <cmohamed992@gmail.com>
2024-02-18 16:13:56 +0100

    create page skils complete

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import About from "@/components/About";
 import Hero from "@/components/Hero";
 import MobileNav from "@/components/MobileNav";
 import Nav from "@/components/Nav";
+import Services from "@/components/Services";
 import React, { useState } from "react";
 
 const HomePage = () => {
@@ -19,6 +20,8 @@ const HomePage = () => {
         <Hero />
         <div className="relative z-[30]">
           <About />
+          {/* Services */}
+          <Services />
         </div>
       </div>
     </div>
